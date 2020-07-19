752a7d4e4e022feadefb36e1f6552c61ba5c8119
Addi <coxonmarc1@gmail.com>
2020-07-19 10:34:58 +0700

    commit before adding trending animations

--- js/main.js
+++ js/main.js
@@ -1,12 +1,20 @@
-let tweetCount = [0, 0, 0, 0]
-
+let tweetCount = [{ header: 'Trending', hash: '#WorldNews', retweets: 2 },
+    { header: 'Trending', hash: '#BLACKPINK', retweets: 3 },
+    { header: 'Trending', hash: '#BlackOutTuesday', retweets: 1 },
+    { header: 'Trending', hash: '#BunBoHue', retweets: 0 }
+]
 
 
 function randomNumTweets() {
 
     for (i = 0; i < tweetCount.length; i++) {
-        tweetCount[i] += Math.round(Math.random() * 10)
-        document.getElementById(`tweetCount${i+1}`).innerHTML = `${tweetCount[i]}k tweets`
+        tweetCount[i].retweets += Math.round((Math.random() * 10))
+
+        sortCount = tweetCount.sort((a, b) => parseFloat(b.retweets) - parseFloat(a.retweets))
+
+        console.log(sortCount)
+
+        document.getElementById(`trendCard${i}`).innerHTML = `<p>1. ${sortCount[i].header}</p><h6>${sortCount[i].hash}</h6><p id="tweetCount1">${sortCount[i].retweets}k Tweets</p>`
     }
 }
 
